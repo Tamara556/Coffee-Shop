535f96d41174430b6d1e3056ee0dd9413347672f
Astghik <astgh.gevorgyan06@gmail.com>
2025-02-12 21:18:17 +0400

    Implement service and repo layer

---
+++ src/main/java/org/example/coffeeshop/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.example.coffeeshop.repository;
+
+import org.example.coffeeshop.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/org/example/coffeeshop/servise/impl/CategoryServiceImpl.java
@@ -0,0 +1,9 @@
+package org.example.coffeeshop.servise.impl;
+
+import org.example.coffeeshop.servise.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    
+}
